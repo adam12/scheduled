8c4677124b893b5fd7764e2454d9fae50c9b7010
Adam Daniels <adam@mediadrive.ca>
2021-04-19 13:41:18 -0400

    Replace module_function with class methods

--- lib/scheduled.rb
+++ lib/scheduled.rb
@@ -5,64 +5,66 @@ require "scheduled/cron_parser"
 module Scheduled
   Job = Struct.new(:last_run)
 
-  module_function
-
-  def every(interval, &block)
-    rescued_block = ->() do
-      begin
-        block.call
-      rescue Exception => e
-        Thread.new { raise e }
-      end
-    end
-
-    if interval.is_a?(Integer)
-      task = Concurrent::TimerTask.new(execution_interval: interval, run_now: true) do
-        rescued_block.call
+  module ClassMethods
+    def every(interval, &block)
+      rescued_block = ->() do
+        begin
+          block.call
+        rescue Exception => e
+          Thread.new { raise e }
+        end
       end
 
-      task.execute
-
-    elsif interval.is_a?(String)
-      run = ->() {
-        parsed_cron = CronParser.new(interval)
-        next_tick_delay = parsed_cron.next(Time.now) - Time.now
-
-        task = Concurrent::ScheduledTask.execute(next_tick_delay) do
+      if interval.is_a?(Integer)
+        task = Concurrent::TimerTask.new(execution_interval: interval, run_now: true) do
           rescued_block.call
-          run.call
         end
 
         task.execute
-      }
 
-      run.call
+      elsif interval.is_a?(String)
+        run = ->() {
+          parsed_cron = CronParser.new(interval)
+          next_tick_delay = parsed_cron.next(Time.now) - Time.now
 
-    elsif interval.respond_to?(:call)
-      job = Job.new
+          task = Concurrent::ScheduledTask.execute(next_tick_delay) do
+            rescued_block.call
+            run.call
+          end
 
-      task = Concurrent::TimerTask.new(execution_interval: 1, run_now: true) do |timer_task|
-        case interval.call(job)
-        when true
-          rescued_block.call
+          task.execute
+        }
 
-          job.last_run = Time.now
-        when :cancel
-          timer_task.shutdown
+        run.call
+
+      elsif interval.respond_to?(:call)
+        job = Job.new
+
+        task = Concurrent::TimerTask.new(execution_interval: 1, run_now: true) do |timer_task|
+          case interval.call(job)
+          when true
+            rescued_block.call
+
+            job.last_run = Time.now
+          when :cancel
+            timer_task.shutdown
+          end
         end
+
+        task.execute
+      else
+        raise ArgumentError, "Unsupported value for interval"
       end
+    end
 
-      task.execute
-    else
-      raise ArgumentError, "Unsupported value for interval"
+    def wait
+      trap("INT") { exit }
+
+      loop do
+        sleep 1
+      end
     end
   end
 
-  def wait
-    trap("INT") { exit }
-
-    loop do
-      sleep 1
-    end
-  end
+  extend ClassMethods
 end
